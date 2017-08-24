7fb34ede2cfad8d646d99d13c9af8d930a7f9d5f
Antoinette Janus <acj.gfx@gmail.com>
2017-08-23 20:53:52 -0400

    Converted to Jekyll static site

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
-  return gulp.src('./scss/*.scss')
+  return gulp.src('./_scss/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(minifycss())
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('_includes'))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('./_scss/*.scss', ['sass']);
 });
